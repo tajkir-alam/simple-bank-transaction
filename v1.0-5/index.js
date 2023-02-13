7db511439223830eb79037b8861e6167e8722aa6
tajkir-alam <tajkir.alam@gmail.com>
2023-02-14 02:28:37 +0600

    folders name modified

---
+++ v1.0-5/index.js
@@ -0,0 +1,47 @@
+document.getElementById('user-email').addEventListener('keyup', function(){
+    const userEmail = document.getElementById('user-email').value;
+    const userPassword = document.getElementById('user-password').value;
+    if(userEmail !== "" && userPassword !== ""){
+        document.getElementById('btn-login').removeAttribute('disabled');
+    }
+    else{
+        document.getElementById('btn-login').setAttribute('disabled', true);
+    }
+})
+
+document.getElementById('user-password').addEventListener('keyup', function(){
+    const userEmail = document.getElementById('user-email').value;
+    const userPassword = document.getElementById('user-password').value;
+    if(userEmail !== "" && userPassword !== ""){
+        document.getElementById('btn-login').removeAttribute('disabled');
+    }
+    else{
+        document.getElementById('btn-login').setAttribute('disabled', true);
+    }
+})
+
+
+document.getElementById('btn-login').addEventListener('click', function(){
+    const userEmail = document.getElementById('user-email').value;
+    const userPassword = document.getElementById('user-password').value;
+    const localUser = localStorage.getItem(userEmail);
+
+    if(userPassword.length < 6 ){
+        document.getElementById('btn-login').setAttribute('disabled', true);
+        const checkPassword = document.createElement('p');
+        checkPassword.className = "text-red-500 font-medium incorrect-password";
+        checkPassword.innerText = "Your have to enter minimum 6 digit password";
+        document.getElementById('login-input-section').appendChild(checkPassword);
+        document.getElementById('user-password').value = "";
+
+        const incorrectPasswordLists = document.querySelectorAll('.incorrect-password');
+        incorrectPasswordLists[1].remove();    
+    }
+    else if(localUser !== userPassword){
+        alert('You enter invalid username or password');
+    }
+    else{
+        sessionStorage.setItem("user", "true");
+        window.location.href = 'dashboard.html';
+    }
+})
--- v1.0/index.js
+++ v1.0/index.js
@@ -1,47 +1,12 @@
-document.getElementById('user-email').addEventListener('keyup', function(){
-    const userEmail = document.getElementById('user-email').value;
-    const userPassword = document.getElementById('user-password').value;
-    if(userEmail !== "" && userPassword !== ""){
-        document.getElementById('btn-login').removeAttribute('disabled');
-    }
-    else{
-        document.getElementById('btn-login').setAttribute('disabled', true);
-    }
-})
+// document.getElementById('btn-login').addEventListener('click', function(){
+//     const inputUserName = document.getElementById('input-userName');
+//     const inputPassword = document.getElementById('input-password');
 
-document.getElementById('user-password').addEventListener('keyup', function(){
-    const userEmail = document.getElementById('user-email').value;
-    const userPassword = document.getElementById('user-password').value;
-    if(userEmail !== "" && userPassword !== ""){
-        document.getElementById('btn-login').removeAttribute('disabled');
-    }
-    else{
-        document.getElementById('btn-login').setAttribute('disabled', true);
-    }
-})
+//     if(inputUserName.value === "rion" && inputPassword.value === "tkuthabo" && inputPassword.value.length > 6){
+//         window.location.href = "dashboard.html";
+//     }
+//     else{
+//         alert('Check your Username and Password again...');
+//     }
+// });
 
-
-document.getElementById('btn-login').addEventListener('click', function(){
-    const userEmail = document.getElementById('user-email').value;
-    const userPassword = document.getElementById('user-password').value;
-    const localUser = localStorage.getItem(userEmail);
-
-    if(userPassword.length < 6 ){
-        document.getElementById('btn-login').setAttribute('disabled', true);
-        const checkPassword = document.createElement('p');
-        checkPassword.className = "text-red-500 font-medium incorrect-password";
-        checkPassword.innerText = "Your have to enter minimum 6 digit password";
-        document.getElementById('login-input-section').appendChild(checkPassword);
-        document.getElementById('user-password').value = "";
-
-        const incorrectPasswordLists = document.querySelectorAll('.incorrect-password');
-        incorrectPasswordLists[1].remove();    
-    }
-    else if(localUser !== userPassword){
-        alert('You enter invalid username or password');
-    }
-    else{
-        sessionStorage.setItem("user", "true");
-        window.location.href = 'dashboard.html';
-    }
-})
