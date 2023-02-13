2ea3f5e842024a2aa9bb9910ead7ff80926fbc12
tajkir-alam <tajkir.alam@gmail.com>
2023-02-13 23:06:17 +0600

    disabled attribute prob solved

--- v1.0/signup.js
+++ v1.0/signup.js
@@ -1,16 +1,33 @@
-document.getElementById('btn-signup').addEventListener('click', function(){
+
+document.getElementById('signup-email').addEventListener('keyup', function(){
     const signupEmail = document.getElementById('signup-email').value;
-    
-    
-    console.log(signupEmail);
-
-
     const signupPassword = document.getElementById('signup-password').value;
 
-    if(signupEmail === "" || signupPassword === ""){
-        alert('Please Provide email and password');
+    if(signupEmail !== "" && signupPassword !== ""){
+        document.getElementById('btn-signup').removeAttribute('disabled');
     }
-    else if(signupPassword.length < 6 ){
+    else{
+        document.getElementById('btn-signup').setAttribute('disabled', true);
+    }
+})
+document.getElementById('signup-password').addEventListener('keyup', function(){
+    const signupEmail = document.getElementById('signup-email').value;
+    const signupPassword = document.getElementById('signup-password').value;
+
+    if(signupEmail !== "" && signupPassword !== ""){
+        document.getElementById('btn-signup').removeAttribute('disabled');
+    }
+    else{
+        document.getElementById('btn-signup').setAttribute('disabled', true);
+    }
+})
+
+
+
+document.getElementById('btn-signup').addEventListener('click', function(){
+    const signupEmail = document.getElementById('signup-email').value;
+    const signupPassword = document.getElementById('signup-password').value;
+    if(signupPassword.length < 6 ){
         const checkPassword = document.createElement('p');
         checkPassword.className = "text-red-500 font-medium";
         checkPassword.innerText = "Your have to enter minimum 6 digit password";
@@ -18,6 +35,7 @@ document.getElementById('btn-signup').addEventListener('click', function(){
     }
     else{
         window.location.href = "index.html";
+        window.localStorage.setItem(signupEmail, signupPassword)
     }
 })
 
