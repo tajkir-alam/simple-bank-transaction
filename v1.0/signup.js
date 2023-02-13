b17569c7e23e72b5fd48f75b0722242a2b89245c
tajkir-alam <tajkir.alam@gmail.com>
2023-02-13 22:10:57 +0600

    third commit

---
+++ v1.0/signup.js
@@ -0,0 +1,19 @@
+document.getElementById('btn-signup').addEventListener('click', function(){
+    const signupEmail = document.getElementById('signup-email').value;
+    console.log(signupEmail);
+    const signupPassword = document.getElementById('signup-password').value;
+
+    if(signupEmail === "" || signupPassword === ""){
+        alert('Please Provide email and password');
+    }
+    else if(signupPassword.length < 6 ){
+        const checkPassword = document.createElement('p');
+        checkPassword.className = "text-red-500 font-medium";
+        checkPassword.innerText = "Your have to enter minimum 6 digit password";
+        document.getElementById('signup-input-section').appendChild(checkPassword);
+    }
+    else{
+        window.location.href = "index.html";
+    }
+})
+
