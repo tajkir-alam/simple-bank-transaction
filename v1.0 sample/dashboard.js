3c67a9922e9f5b0c9e70494b3cad188824bc55f4
tajkir-alam <tajkir.alam@gmail.com>
2023-02-13 20:52:00 +0600

    first commit

---
+++ v1.0 sample/dashboard.js
@@ -0,0 +1,74 @@
+// document.getElementById('submit-deposit').addEventListener('click', function(){
+//     const depositValueString = document.getElementById('deposit-value');
+//     // const depositValueString1 = depositValueString.value;
+//     const depositValue = parseFloat(depositValueString.value);
+
+//     const depositAmountString = document.getElementById('deposit-amount');
+//     // const depositAmountString1 = depositAmountString.innerText;
+//     const depositAmount = parseFloat(depositAmountString.innerText);
+
+
+//     const deposit = depositValue + depositAmount;
+//     depositAmountString.innerText = deposit;
+//     depositValueString.value = "";
+// })
+
+
+
+
+
+
+// // Practicing again the same thing
+
+// // For Deposit
+// document.getElementById('submit-deposit').addEventListener('click', function(){
+//     const depositValueString = document.getElementById('deposit-value');
+//     const depositValue = parseFloat(depositValueString.value);
+
+//     const depositTotalString = document.getElementById('deposit-total');
+//     const depositTotal = parseFloat(depositTotalString.innerText);
+
+//     const deposit = depositValue + depositTotal;
+//     depositTotalString.innerText = deposit;
+    
+//     const balanceTotalString = document.getElementById('balance-total');
+//     const balanceTotal = parseFloat(balanceTotalString.innerText);
+//     const balance = depositValue + balanceTotal;
+//     balanceTotalString.innerText = balance;
+    
+    
+    
+//     depositValueString.value = "";
+// })
+
+
+
+
+// // For withdraw
+// document.getElementById('submit-withdraw').addEventListener('click', function(){
+//     const withdrawValueString = document.getElementById('withdraw-value');
+//     const withdrawValue = parseFloat(withdrawValueString.value);
+//     const withdrawTotalString = document.getElementById('withdraw-total');
+//     const withdrawTotal = parseFloat(withdrawTotalString.innerText);
+
+//     const withdraw = withdrawValue + withdrawTotal;
+//     withdrawTotalString.innerText = withdraw;
+
+//     const balanceTotalString = document.getElementById('balance-total');
+//     const balanceTotal = parseFloat(balanceTotalString.innerText);
+//     const balance = balanceTotal - withdrawValue;
+//     balanceTotalString.innerText = balance;
+
+//     withdrawValueString.value = "";
+// })
+
+
+
+
+// document.getElementById('logout-btn').addEventListener('click', function(){
+//     window.location.href = 'index.html';
+// })
+
+
+
+
---
+++ v1.0 sample/index.js
@@ -0,0 +1,12 @@
+// document.getElementById('btn-login').addEventListener('click', function(){
+//     const inputUserName = document.getElementById('input-userName');
+//     const inputPassword = document.getElementById('input-password');
+
+//     if(inputUserName.value === "rion" && inputPassword.value === "tkuthabo" && inputPassword.value.length > 6){
+//         window.location.href = "dashboard.html";
+//     }
+//     else{
+//         alert('Check your Username and Password again...');
+//     }
+// });
+
