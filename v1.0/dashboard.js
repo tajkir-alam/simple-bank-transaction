46e9b2dead64bfd361e78587d75cce0c2dcdfcf0
tajkir-alam <tajkir.alam@gmail.com>
2023-02-14 09:21:20 +0600

    v1.0 unComment

--- v1.0/dashboard.js
+++ v1.0/dashboard.js
@@ -1,73 +1,64 @@
-// document.getElementById('submit-deposit').addEventListener('click', function(){
-//     const depositValueString = document.getElementById('deposit-value');
-//     // const depositValueString1 = depositValueString.value;
-//     const depositValue = parseFloat(depositValueString.value);
+document.getElementById('submit-deposit').addEventListener('click', function(){
+    const depositValueString = document.getElementById('deposit-value');
+    // const depositValueString1 = depositValueString.value;
+    const depositValue = parseFloat(depositValueString.value);
 
-//     const depositAmountString = document.getElementById('deposit-amount');
-//     // const depositAmountString1 = depositAmountString.innerText;
-//     const depositAmount = parseFloat(depositAmountString.innerText);
-
-
-//     const deposit = depositValue + depositAmount;
-//     depositAmountString.innerText = deposit;
-//     depositValueString.value = "";
-// })
+    const depositAmountString = document.getElementById('deposit-amount');
+    // const depositAmountString1 = depositAmountString.innerText;
+    const depositAmount = parseFloat(depositAmountString.innerText);
 
 
+    const deposit = depositValue + depositAmount;
+    depositAmountString.innerText = deposit;
+    depositValueString.value = "";
+})
 
 
+// Practicing again the same thing
 
+// For Deposit
+document.getElementById('submit-deposit').addEventListener('click', function(){
+    const depositValueString = document.getElementById('deposit-value');
+    const depositValue = parseFloat(depositValueString.value);
 
-// // Practicing again the same thing
+    const depositTotalString = document.getElementById('deposit-total');
+    const depositTotal = parseFloat(depositTotalString.innerText);
 
-// // For Deposit
-// document.getElementById('submit-deposit').addEventListener('click', function(){
-//     const depositValueString = document.getElementById('deposit-value');
-//     const depositValue = parseFloat(depositValueString.value);
-
-//     const depositTotalString = document.getElementById('deposit-total');
-//     const depositTotal = parseFloat(depositTotalString.innerText);
-
-//     const deposit = depositValue + depositTotal;
-//     depositTotalString.innerText = deposit;
+    const deposit = depositValue + depositTotal;
+    depositTotalString.innerText = deposit;
     
-//     const balanceTotalString = document.getElementById('balance-total');
-//     const balanceTotal = parseFloat(balanceTotalString.innerText);
-//     const balance = depositValue + balanceTotal;
-//     balanceTotalString.innerText = balance;
+    const balanceTotalString = document.getElementById('balance-total');
+    const balanceTotal = parseFloat(balanceTotalString.innerText);
+    const balance = depositValue + balanceTotal;
+    balanceTotalString.innerText = balance;    
     
-    
-    
-//     depositValueString.value = "";
-// })
+    depositValueString.value = "";
+})
 
 
 
+// For withdraw
+document.getElementById('submit-withdraw').addEventListener('click', function(){
+    const withdrawValueString = document.getElementById('withdraw-value');
+    const withdrawValue = parseFloat(withdrawValueString.value);
+    const withdrawTotalString = document.getElementById('withdraw-total');
+    const withdrawTotal = parseFloat(withdrawTotalString.innerText);
 
-// // For withdraw
-// document.getElementById('submit-withdraw').addEventListener('click', function(){
-//     const withdrawValueString = document.getElementById('withdraw-value');
-//     const withdrawValue = parseFloat(withdrawValueString.value);
-//     const withdrawTotalString = document.getElementById('withdraw-total');
-//     const withdrawTotal = parseFloat(withdrawTotalString.innerText);
+    const withdraw = withdrawValue + withdrawTotal;
+    withdrawTotalString.innerText = withdraw;
 
-//     const withdraw = withdrawValue + withdrawTotal;
-//     withdrawTotalString.innerText = withdraw;
+    const balanceTotalString = document.getElementById('balance-total');
+    const balanceTotal = parseFloat(balanceTotalString.innerText);
+    const balance = balanceTotal - withdrawValue;
+    balanceTotalString.innerText = balance;
 
-//     const balanceTotalString = document.getElementById('balance-total');
-//     const balanceTotal = parseFloat(balanceTotalString.innerText);
-//     const balance = balanceTotal - withdrawValue;
-//     balanceTotalString.innerText = balance;
-
-//     withdrawValueString.value = "";
-// })
-
+    withdrawValueString.value = "";
+})
 
 
-
-// document.getElementById('logout-btn').addEventListener('click', function(){
-//     window.location.href = 'index.html';
-// })
+document.getElementById('logout-btn').addEventListener('click', function(){
+    window.location.href = 'index.html';
+})
 
 
 
