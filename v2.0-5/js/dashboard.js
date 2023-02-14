a7b0f07945a4ab30a0782ed1f719445d5a443c43
tajkir-alam <tajkir.alam@gmail.com>
2023-02-14 12:49:05 +0600

    deposit input done

---
+++ v2.0-5/js/dashboard.js
@@ -0,0 +1,6 @@
+document.getElementById('deposit-value').addEventListener('keyup', function(){
+    activeButton('deposit-value', 'deposit-btn');
+})
+document.getElementById('withdraw-value').addEventListener('keyup', function(){
+    activeButton('withdraw-value', 'withdraw-btn');
+})
--- v2.0-5/js/utilities.js
+++ v2.0-5/js/utilities.js
@@ -1,4 +1,5 @@
-// User Validation section
+// ----->>>>> User Validation section
+
 function userValidation(){
     const userEmail = document.getElementById('user-email').value;
     const userPassword = document.getElementById('user-password').value;
@@ -18,4 +19,21 @@ function passwordValidation(){
     p.innerText = "You have to provide minimum 6 digit password";
     document.getElementById('authintication-section').appendChild(p);
     document.querySelectorAll('p')[1].remove();
+}
+
+
+
+// ----->>>>> Dashboard calculation
+
+function activeButton(inputValue, btnName){
+    const inputValueString = document.getElementById(inputValue).value;
+    const inputValueNumber = parseFloat(inputValueString);
+    if(inputValueString !== ""){
+        document.getElementById(btnName).removeAttribute('disabled');
+        return;
+    }
+    else{
+        document.getElementById(btnName).setAttribute('disabled', true);
+        return;
+    }
 }
